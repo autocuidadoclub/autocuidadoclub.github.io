87c4708ae2034a60ee3799412dc0f6f9eb7f35c9
autocuidadoclub <cesarstanley.1992@gmail.com>
2024-10-31 11:27:25 -0600

    Update login.js

--- login.js
+++ login.js
@@ -1,17 +1,4 @@
-// login.js
-
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
-const firebaseConfig = {
-  apiKey: "AIzaSyBsYS6pHWaTgXzdml-H_y3lQewWgOUezPM",
-  authDomain: "auto-cuidadoclub.firebaseapp.com",
-  databaseURL: "https://auto-cuidadoclub-default-rtdb.firebaseio.com",
-  projectId: "auto-cuidadoclub",
-  storageBucket: "auto-cuidadoclub.appspot.com",
-  messagingSenderId: "986704701191",
-  appId: "1:986704701191:web:fc96ef678d64c1cdcf47a2",
-  measurementId: "G-LBBGXV2YX5"
+import { auth, signInWithEmailAndPassword } from './firebase.js';
 
 document.getElementById('loginForm').addEventListener('submit', (e) => {
     e.preventDefault();
@@ -19,11 +6,9 @@ document.getElementById('loginForm').addEventListener('submit', (e) => {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    firebase.auth().signInWithEmailAndPassword(email, password)
+    signInWithEmailAndPassword(auth, email, password)
         .then(() => {
             window.location.href = 'profile.html';
         })
-        .catch((error) => {
-            console.error('Error al iniciar sesión:', error);
-        });
+        .catch(error => console.error('Error al iniciar sesión:', error));
 });
