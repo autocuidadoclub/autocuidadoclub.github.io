6b0c2dd889abcb7d9478913f09a50468b9fcde02
autocuidadoclub <cesarstanley.1992@gmail.com>
2025-02-07 18:50:06 -0600

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-echo 'const functions = require("firebase-functions");
-
-exports.processSubscription = functions.https.onRequest((req, res) => {
-  res.send("Cloud Function Deployed Successfully!");
-});' > index.js
