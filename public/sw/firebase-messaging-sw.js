0fc5d0bdee73fbc03d50e881fe44bb430517c32d
autocuidadoclub <cesarstanley.1992@gmail.com>
2025-03-31 15:30:04 -0600

    Update firebase-messaging-sw.js

--- public/sw/firebase-messaging-sw.js
+++ public/sw/firebase-messaging-sw.js
@@ -5,7 +5,7 @@ firebase.initializeApp({
   apiKey: "AIzaSyCY6BeuJSbUboosMDrvlzQUmeNLb2dJX_0",
   authDomain: "bookings-autocuidad-club.firebaseapp.com",
   projectId: "bookings-autocuidad-club",
-  storageBucket: "bookings-autocuidad-club.firebasestorage.app",
+  storageBucket: "bookings-autocuidad-club.appspot.com",
   messagingSenderId: "179075180686",
   appId: "1:179075180686:web:6950b87b7fd6c6f49d01df",
   measurementId: "G-LXJ50PGESY"
