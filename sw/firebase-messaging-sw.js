99efe1ad53b87f5ed94545566c0973ff5dca8175
autocuidadoclub <cesarstanley.1992@gmail.com>
2025-04-02 01:09:28 +0000

    Deploying to gh-pages from @ autocuidadoclub/autocuidadoclub.github.io@2219ac70ce7fd4d5155d02d5b6540b7b3922ede1 🚀

--- sw/firebase-messaging-sw.js
+++ sw/firebase-messaging-sw.js
@@ -4,13 +4,13 @@ importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging.js');
 
 // Initialize Firebase in the service worker
 firebase.initializeApp({
-    apiKey: "AIzaSyCY6BeuJSbUboosMDrvlzQUmeNLb2dJX_0",
-    authDomain: "bookings-autocuidad-club.firebaseapp.com",
-    projectId: "bookings-autocuidad-club",
-    storageBucket: "bookings-autocuidad-club.firebasestorage.app",
-    messagingSenderId: "179075180686",
-    appId: "1:179075180686:web:6950b87b7fd6c6f49d01df",
-    measurementId: "G-LXJ50PGESY"
+  apiKey: "AIzaSyCY6BeuJSbUboosMDrvlzQUmeNLb2dJX_0",
+  authDomain: "bookings-autocuidad-club.firebaseapp.com",
+  projectId: "bookings-autocuidad-club",
+  storageBucket: "bookings-autocuidad-club.firebasestorage.app",
+  messagingSenderId: "179075180686",
+  appId: "1:179075180686:web:6950b87b7fd6c6f49d01df",
+  measurementId: "G-LXJ50PGESY"
 });
 
 // Retrieve an instance of Firebase Messaging
@@ -18,12 +18,12 @@ const messaging = firebase.messaging();
 
 // Handle background push notifications
 messaging.onBackgroundMessage(function(payload) {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload);
-    const notificationTitle = 'Background Message Title';
-    const notificationOptions = {
-        body: 'Background Message body.',
-        icon: '/firebase-logo.png'  // Ensure this icon exists at the root
-    };
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png'  // Make sure this icon exists at the root
+  };
 
-    self.registration.showNotification(notificationTitle, notificationOptions);
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
