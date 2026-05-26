769a1480ed4ba3329b082724d02fee5b8280c405
autocuidadoclub <cesarstanley.1992@gmail.com>
2026-05-26 08:01:40 -0600

    Update send.js

--- whatsapp-bulk/send.js
+++ whatsapp-bulk/send.js
@@ -34,15 +34,17 @@ client.on('ready', async () => {
         const number = contact.telefono + '@c.us';
 
         // Select message based on file name
-        const message = esAgua
-          ? `Hola ${contact.nombre}, buen día 👋\n\n` +
-            `¿Necesitan agua Cristal de 5 galones para esta semana? 💧\n\n` +
-            `🚚 Entrega a domicilio\n` +
-            `Quedo atento a su pedido. ¡Gracias!`
-          : `Hola ${contact.nombre}, buen día 👋\n\n` +
-            `¿Necesitan hielo de 50 lb para esta semana? 🧊\n\n` +
-            `🚚 Entrega a domicilio\n` +
-            `Quedo atento a su pedido. ¡Gracias!`;
+       const message = esAgua
+  ? `Hola ${contact.nombre}, buen día 👋\n\n` +
+    `¿Necesitan agua Cristal de 5 galones para esta semana? 💧\n\n` +
+    `🚚 Entrega a domicilio\n` +
+    `Quedo atento a su pedido. ¡Gracias!`
+  : `Hola ${contact.nombre}, buen día 👋\n\n` +
+    `Recuerden que con buen hielo sus bebidas se mantienen frías por más tiempo y mejoran la experiencia de sus clientes 🧊🍹\n\n` +
+    `¿Necesitan hielo de 50 lb para esta semana?\n\n` +
+    `🚚 Entrega a domicilio\n` +
+    `💎 Más calidad para sus bebidas y eventos\n\n` +
+    `Quedo atento a su pedido. ¡Gracias!`;
 
         try {
          const chat = await client.getChatById(number);
